9c444207e31aa528d42543216397067a6de687e2
Alexander Treib <alextreib94@gmail.com>
2018-04-15 21:28:00 +0200

    adding better weights_init

--- LeNet-Lab.py
+++ LeNet-Lab.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 import tensorflow as tf
-from tensorflow.contrib.layers import flatten
+from tensorflow.contrib.layers import flatten, xavier_initializer_conv2d
 
 # Input data
 mnist = input_data.read_data_sets("MNIST_data/", reshape=False)
@@ -21,106 +21,76 @@ X_train      = np.pad(X_train, ((0,0),(2,2),(2,2),(0,0)), 'constant')
 X_validation = np.pad(X_validation, ((0,0),(2,2),(2,2),(0,0)), 'constant')
 X_test       = np.pad(X_test, ((0,0),(2,2),(2,2),(0,0)), 'constant')
 
-index = random.randint(0, len(X_train))
-image = X_train[index].squeeze()
+# index = random.randint(0, len(X_train))
+# image = X_train[index].squeeze()
 
-X_train, y_train = shuffle(X_train, y_train)
+# X_train, y_train = shuffle(X_train, y_train)
 
-TrainingData=[X_train,y_train]
-ValidationData=[X_validation,y_validation]
-TestData=[X_test,y_test]
-
-# Fixed parameters
+# Üarameters
 EPOCHS = 10
-BATCH_SIZE = 128
+BATCH_SIZE = 80
 
-# CNN parameters
-rate = 0.001
+learning_rate = 0.001
+
 
 def LeNet(x):    
-    # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
+   # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
     mu = 0
     sigma = 0.1
     
-    #Weights
     weights = {
-        'f1': tf.Variable(tf.truncated_normal([5,5, 1, 6])),
-        'f2': tf.Variable(tf.truncated_normal([5,5, 6, 16])),
-        'f3': tf.Variable(tf.truncated_normal([5*5*16,400]))}
+        'f1': tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean = mu, stddev = sigma)),
+        'f2': tf.Variable(tf.truncated_normal(shape=(5,5, 6, 16), mean = mu, stddev = sigma)),
+        'f3': tf.Variable(tf.truncated_normal(shape=(5*5*16,400), mean = mu, stddev = sigma))}
 
     biases = {
         'b1': tf.Variable(tf.truncated_normal([6])),
         'b2': tf.Variable(tf.truncated_normal([16])),
         'b3': tf.Variable(tf.truncated_normal([400]))}
     
-    conv_layer = tf.nn.conv2d(x, weights['f1'], strides=[1,1,1,1], padding='VALID')
-    conv_layer = tf.nn.bias_add(conv_layer, biases['b1'])
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean = mu, stddev = sigma))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
 
-    conv_layer = tf.nn.relu(conv_layer)
-    
-    conv_layer = tf.nn.max_pool(
-        conv_layer,
-        ksize=[1,2,2,1],
-        strides=[1,2,2,1],
-        padding='VALID')
-    
-    conv_layer = tf.nn.conv2d(conv_layer, weights['f2'], strides=[1,1,1,1], padding='VALID')
-    conv_layer = tf.nn.bias_add(conv_layer, biases['b2'])
+    # SOLUTION: Activation.
+    conv1 = tf.nn.relu(conv1)
 
-    conv_layer = tf.nn.relu(conv_layer)
+    # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
-    conv_layer = tf.nn.max_pool(
-        conv_layer,
-        ksize=[1,2,2,1],
-        strides=[1,2,2,1],
-        padding='VALID')
+    # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
+    conv2_b = tf.Variable(tf.zeros(16))
+    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    
+    # SOLUTION: Activation.
+    conv2 = tf.nn.relu(conv2)
 
-    conv_layer=tf.contrib.layers.flatten(conv_layer)
+    # SOLUTION: Pooling. Input = 10x10x16. Output = 5x5x16.
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    conv_layer   = flatten(conv2)
+
+    for i_fulCon in range(7):
+        conv_layer=tf.contrib.layers.fully_connected(conv_layer,400-i_fulCon*65,weights_initializer=xavier_initializer_conv2d(uniform=False))
+        print (400-i_fulCon*65)
     
-    conv_layer=tf.contrib.layers.fully_connected(conv_layer,120)
-   
-    conv_layer=tf.contrib.layers.fully_connected(conv_layer,84)
-    
-    conv_layer=tf.contrib.layers.fully_connected(conv_layer,10)
     return conv_layer
 
 x = tf.placeholder(tf.float32, (None, 32, 32, 1))
 y = tf.placeholder(tf.int32, (None))
 one_hot_y = tf.one_hot(y, 10)
 
+logits = LeNet(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+training_operation = optimizer.minimize(loss_operation)
 
 
-def train(TrainingData, ValidationData, TestData):
-    X_train=TrainingData[0]
-    y_train=TrainingData[1]
-
-    num_examples = len(X_train)
-    logits = LeNet(x)
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
-    loss_operation = tf.reduce_mean(cross_entropy)
-    optimizer = tf.train.AdamOptimizer(learning_rate = rate)
-    training_operation = optimizer.minimize(loss_operation)
-
-    print("Training...")
-    for i in range(EPOCHS):
-        X_train, y_train = shuffle(X_train, y_train)
-        for offset in range(0, num_examples, BATCH_SIZE):
-            end = offset + BATCH_SIZE
-            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
-            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
-            
-        validation_accuracy = evaluate(ValidationData)
-        print("EPOCH {} ...".format(i+1))
-        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
-
-
-def evaluate(ValidationData):
-    X_data=ValidationData[0]
-    y_data=ValidationData[1]
-
+def evaluate(X_data, y_data):
     correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
     accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    saver = tf.train.Saver()
     num_examples = len(X_data)
     total_accuracy = 0
     sess = tf.get_default_session()
@@ -132,6 +102,16 @@ def evaluate(ValidationData):
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    train()
+    num_examples = len(X_train)
     
-    
+    print("Training...")
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+            
+        validation_accuracy = evaluate(X_validation, y_validation)
+        print("EPOCH {} ...".format(i+1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
